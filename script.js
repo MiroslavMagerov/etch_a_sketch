053f0f8d0fc390988290cc9b361ea8f3f1c008e8
Mirko <miroslav.magerov@gmail.com>
2023-12-11 11:21:44 +0100

    Ficheros base creados

---
+++ script.js
@@ -0,0 +1,7 @@
+generalDiv = document.querySelector("#general_div");
+
+gridDiv = document.createElement("div");
+gridDiv.style.width = "16px";
+gridDiv.style.heigth = "16px";
+
+generalDiv.append(gridDiv);
