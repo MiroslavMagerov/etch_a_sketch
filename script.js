edac4429a21db45a07ddc8e3de8b8cfc197ce233
Mirko <miroslav.magerov@gmail.com>
2024-02-07 22:04:38 +0100

    Etch a sketch completed

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-generalDiv = document.querySelector("#general_div");
+function init() {
+    const SQUARE_SIZE = 30;
+    const GENERAL_DIV = document.querySelector("#general-div");
 
-gridDiv = document.createElement("div");
-gridDiv.style.width = "16px";
-gridDiv.style.heigth = "16px";
+    createGrid(SQUARE_SIZE, GENERAL_DIV);
+}
 
-generalDiv.append(gridDiv);
+function createGrid(size, container) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let GRID_DIV = document.createElement("div");
+            GRID_DIV.classList.add("grid-div");
+            GRID_DIV.style.width = `calc(100%/${size})`;
+            GRID_DIV.style.height = `calc(100%/${size})`;
+            container.append(GRID_DIV);
+        }
+    }
+}
